cd8e922c3953e8bb8c0c151c3d23fa6225cd8481
matemikus <c21379483@mytudublin.ie>
2023-02-22 15:54:08 +0000

    changes

--- java/src/ie/tudublin/Audio1.java
+++ java/src/ie/tudublin/Audio1.java
@@ -8,34 +8,55 @@ import processing.core.PApplet;
 
 public class Audio1 extends PApplet
 {
-
     Minim minim;
-    AudioInput ai;
     AudioPlayer ap;
+    AudioInput ai;
     AudioBuffer ab;
 
-	public void settings()
-	{
-		size(1024, 500);
+    int mode = 0;
+
+    float y = 0;
+    float smoothedY = 0;
+    float smoothedAmplitude = 0;
+
+    public void keyPressed() {
+		if (key >= '0' && key <= '9') {
+			mode = key - '0';
+		}
+		if (keyCode == ' ') {
+            if (ap.isPlaying()) {
+                ap.pause();
+            } else {
+                ap.rewind();
+                ap.play();
+            }
+        }
 	}
 
-    int frameSize = 1024;
+    public void settings()
+    {
+        size(1024, 1000, P3D);
+        //fullScreen(P3D, SPAN);
+    }
 
-	public void setup() {
-		colorMode(HSB);
-		background(0);
-
+    public void setup()
+    {
         minim = new Minim(this);
+        // Uncomment this to use the microphone
+        ai = minim.getLineIn(Minim.MONO, width, 44100, 16);
+        ab = ai.mix; 
 
-        ai = minim.getLineIn(Minim.MONO, frameSize, 44100, 16);
-        ab = ai.mix;
-		
-		smooth();
-		
-	}
+        // And comment the next two lines out
+        //ap = minim.loadFile("heroplanet.mp3", 1024);
+        //ap.play();
+        //ab = ap.mix;
+        colorMode(HSB);
 
-	
-	
+        y = height / 2;
+        smoothedY = y;
+
+    }
+
 	public void draw()
 	{	
 		background(0);
@@ -43,26 +64,12 @@ public class Audio1 extends PApplet
 
         float half = height / 2;
         float cgap = 255 / (float)ab.size();
-
-        float total = 0;
         for(int i = 0 ; i < ab.size() ; i ++)
         {
-            total += abs(ab.get(i)); // get every elemet of the array
             stroke(cgap * i, 255, 255);
             line(i, half, i , half + ab.get(i) * half); 
         }
-        float average = total / (float) ab.size();
-
-        float r = average * 200;
-
-        lerpedR = lerp(lerpedR, r, 0.1f);
-
-        circle(100,200,lerpedR);
-
-        // Lurp algorithm to spoothen it
 
         
 	}
-
-    float lerpedR = 0;
 }
