846d0bc65c16857746e3b3ccb4e73929444fa987
matemikus <c21379483@mytudublin.ie>
2023-02-22 15:38:13 +0000

    Did render

--- java/src/ie/tudublin/Star.java
+++ java/src/ie/tudublin/Star.java
@@ -1,5 +1,6 @@
 package ie.tudublin;
 
+import processing.core.PApplet;
 import processing.data.TableRow;
 
 public class Star {
@@ -33,6 +34,22 @@ public class Star {
             row.getFloat("Distance")
         );
 
+    }
+
+    public void render(PApplet p)
+    {
+        float border = p.width * 0.1f;
+        float x = PApplet.map(xG, -5, 5, border, p.width - border);
+        float y = PApplet.map(yG, -5, 5, border, p.height - border);
+        p.stroke(255,255,0);
+        p.line(x,y-5,x, y+5);
+        p.line(x-5,y,x+5,y+5);
+        p.stroke(255,0,0);
+        p.circle(x,y,absMag);
+        p.fill(255);
+        p.textAlign(PApplet.LEFT, PApplet.CENTER);
+        p.text(displayName, x + 50, y);
+        p.noFill();
     }
 
     public boolean isHab() {
