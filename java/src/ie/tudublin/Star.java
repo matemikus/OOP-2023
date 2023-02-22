77a6ce95e425c9d1c53b7ba6f408f59fa094da49
matemikus <c21379483@mytudublin.ie>
2023-02-22 14:54:19 +0000

    added getters

--- java/src/ie/tudublin/Star.java
+++ java/src/ie/tudublin/Star.java
@@ -9,4 +9,42 @@ public class Star {
     private float absMag;
     private float distance;
 
+    public Star(boolean hab, String displayName, float xG, float yG, float zG, float absMag, float distance) {
+        this.hab = hab;
+        this.displayName = displayName;
+        this.xG = xG;
+        this.yG = yG;
+        this.zG = zG;
+        this.absMag = absMag;
+        this.distance = distance;
+    }
+
+    public boolean isHab() {
+        return hab;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public float getxG() {
+        return xG;
+    }
+
+    public float getyG() {
+        return yG;
+    }
+
+    public float getzG() {
+        return zG;
+    }
+
+    public float getAbsMag() {
+        return absMag;
+    }
+
+    public float getDistance() {
+        return distance;
+    }
+
 }
